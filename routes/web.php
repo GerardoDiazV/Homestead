7b94ffbafdd3efab7643996e2b42e104154489c7
GerardoDiazV <gdv008@alumnos.ucn.cl>
2019-01-09 15:03:49 -0300

    Login Otra vez

--- routes/web.php
+++ routes/web.php
@@ -39,28 +39,22 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 //Acceso de todos los roles (Academico(user),Secretaria(secretaria),Encargado de Vinculacion(encargado), Jefe de Carrera o Director(admin))
-Route::prefix('Encargado-Vinculacion')->group(function(){
-
+Route::middleware(['auth', 'role:encargado' || 'role:secretaria' || 'role:user' || 'role=admin'])->group(function () {
     Route::get('/registros', 'RegistroController@menu')->name('menuRegistros');
+    //Si el Usuario no es Jefe de Carrera o Director
+    Route::middleware(['auth', 'role:encargado' || 'role:secretaria' || 'role:user'])->group(function () {
+        //Administracion de Convenios (REG-001)
+        Route::get('/registroConvenio', ['uses' => 'ConvenioController@create'])->name('registroConvenio');
+        Route::post('/registroConvenio', ['uses' => 'ConvenioController@store']);
 
-    Route::middleware(['auth', 'role:encargado' || 'role:secretaria' || 'role:user' || 'role=admin'])->group(function () {
+        //Administracion de Actividad de Extencsion
+        Route::get('/registroExtension', 'ActividadExtensionController@create')->name('registroExtension');
+        Route::post('/registroExtension', 'ActividadExtensionController@store');
+        Route::get('/registroExtension/{id}/editar', 'ActividadExtensionController@edit');
 
-        //Si el Usuario no es Jefe de Carrera o Director
-        Route::middleware(['auth', 'role:encargado' || 'role:secretaria' || 'role:user'])->group(function () {
-            //Administracion de Convenios (REG-001)
-            Route::get('/registroConvenio', ['uses' => 'ConvenioController@create'])->name('registroConvenio');
-            Route::post('/registroConvenio', ['uses' => 'ConvenioController@store']);
-
-            //Administracion de Actividad de Extencsion
-            Route::get('/registroExtension', 'ActividadExtensionController@create')->name('registroExtension');
-            Route::post('/registroExtension', 'ActividadExtensionController@store');
-            Route::get('/registroExtension/{id}/editar', 'ActividadExtensionController@edit');
-
-            //Administracion de Actividades de Aprendizaje + Servicios (A+S)
-            Route::get('/registroASP', 'ActividadASPController@create')->name('registroASP');
-            Route::post('/registroASP', 'ActividadASPController@store');
-        });
-
+        //Administracion de Actividades de Aprendizaje + Servicios (A+S)
+        Route::get('/registroASP', 'ActividadASPController@create')->name('registroASP');
+        Route::post('/registroASP', 'ActividadASPController@store');
     });
 });
 
