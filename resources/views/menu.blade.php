24379c78757928d4ebdae5ba101a42c4b3302699
GerardoDiazV <gdv008@alumnos.ucn.cl>
2019-01-09 19:12:44 -0300

    update

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -7,7 +7,7 @@
     <head>
         <style>
             .ancho {
-                width: 600px;
+                width: 650px;
             }
         </style>
     </head>
@@ -16,9 +16,9 @@
                 <a href="{{ route('logout') }}"><font color="white">Cerrar Sesión</font></a>
         </div>
         <div class="container ancho p-5">
-            <a class="btn btn-primary btn-block " href="{{route('registroConvenio')}}" role="button"><font size="5">Registrar Convenios de Colaboración</font></a>
-            <a class="btn btn-primary btn-block " href="{{route('registroASP')}}" role="button"><font size="5">Registrar  Actividad de Aprendizaje + Servicios</font></a>
-            <a class="btn btn-primary btn-block " href="{{route('registroExtension')}}" role="button"><font size="5">Registrar Actividad de Extensión</font></a>
+            <a class="btn btn-primary btn-block " href="{{route('registroConvenio')}}" role="button"><font size="5">Administrar Convenios de Colaboración</font></a>
+            <a class="btn btn-primary btn-block " href="{{route('registroASP')}}" role="button"><font size="5">Administrar  Actividad de Aprendizaje + Servicios</font></a>
+            <a class="btn btn-primary btn-block " href="{{route('registroExtension')}}" role="button"><font size="5">Administrar Actividad de Extensión</font></a>
         </div>
     </body>
 </html>
