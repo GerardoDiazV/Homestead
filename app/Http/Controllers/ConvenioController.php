07b7e6ddafff6816b458a396ccdf87fa216b578e
Sebastian Villarroel <sebastian.villarroel.ucn@gmail.com>
2019-01-10 13:10:40 -0300

    Revert "Agregada la validacion"
    This reverts commit 71ee9e10d55061719f0efd31050307bd03725e6c.

--- app/Http/Controllers/ConvenioController.php
+++ app/Http/Controllers/ConvenioController.php
@@ -40,12 +40,6 @@ class ConvenioController extends Controller
     public function store(Request $request)
     {
         $data = request()->all();
-        $ValidateData = $request->validate([
-            'fecha_inicio'  =>'required',
-            'fecha_termino' =>'required',
-            'tipo_convenio' =>'required'
-
-        ]);
 
 
 
