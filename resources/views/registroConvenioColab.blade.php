32216d6a02ed6d0fc31c23f77428a4f9f856dca5
strikereport <34009803+strikereport@users.noreply.github.com>
2019-01-11 00:59:34 -0300

    validacion de convenio
    agregado la validacion de convenio

--- resources/views/registroConvenioColab.blade.php
+++ resources/views/registroConvenioColab.blade.php
@@ -4,7 +4,6 @@
 
     <div>
         <p> Hola mundo este es un nuevo registro</p>
-
         <form action="{{route('registrarConvenio')}}">
             Nombre: <input type="text" name="nombreConvenio"><br>
             Rut: <input type="text" name="rutConvenio"><br>
