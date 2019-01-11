65c994eb64cf886e129f5424edc330368e53f7a3
Sebastian Villarroel <sebastian.villarroel.ucn@gmail.com>
2019-01-11 14:53:13 -0300

    Arreglo Composers

--- config/app.php
+++ config/app.php
@@ -159,8 +159,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Collective\Html\HtmlServiceProvider::class,
-        Freshwork\ChileanBundle\Laravel\ChileanBundleServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -174,7 +172,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -224,8 +221,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
